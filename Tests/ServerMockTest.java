a6fae97090ede3283431ddc74c331345e8a0e321
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2017-11-08 22:34:10 -0300

    Duracion en minutos de vuelos agregado y vuelos agregados a la lista de vuelos del PersonalDeAbordo cuando estos se agragan al Vuelo

--- Tests/ServerMockTest.java
+++ Tests/ServerMockTest.java
@@ -13,7 +13,7 @@ public class ServerMockTest {
         server.addAeropuerto("aaa", "aaa", "aaa");
         server.addTipoDeAvion(1, 1, 1, 1, 1, 1, 1, "a");
         server.addAvion("1", "a");
-        server.addVuelo("aaa", "aaa", 1, 1, 2018, 22, 30, "1", 1, 3);
+        server.addVuelo("aaa", "aaa", 1, 1, 2018, 22, 30,60, "1", 1, 3);
 
 
 
