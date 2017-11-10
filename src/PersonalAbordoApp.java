d633cea261c021be248d98aad1365f752e6cc761
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2017-11-10 07:25:10 -0300

    aaa

--- src/PersonalAbordoApp.java
+++ src/PersonalAbordoApp.java
@@ -17,14 +17,35 @@ public class PersonalAbordoApp {
             iniciarSesion();
         }
         borrarPantalla();
-        mostrarMenu();
+        mostrarMenuAdentro();
     }
+    private void mostrarMenuAfuera(){
+        System.out.println("1- Iniciar Sesion");
+        System.out.println("2-Exit");
 
-    private void mostrarMenu() {
+
+        try{
+            int opcion = Scanner.getInt("Seleccione una opcion: ");
+            switch (opcion){
+                case 1:
+                    iniciarSesion();
+                    break;
+                case 2:
+                    MainApp.mostrarMenu();
+                    break;
+                default:
+                    throw new RuntimeException("Opcion invalida");
+            }
+        }
+        catch (RuntimeException e){
+            System.out.println(e.getMessage());
+            mostrarMenuAfuera();
+        }
+
+    }
+    private void mostrarMenuAdentro() {
         System.out.println("1- Ver itinerario de viajes");
         System.out.println("2- Cerrar sesion");
-        System.out.println("- - - - - - - - - - - - - - - - - -");
-        System.out.println("9- Exit");
         System.out.println();
         try {
             int opcion = Scanner.getInt("Seleccione una opcion: ");
@@ -36,27 +57,23 @@ public class PersonalAbordoApp {
                 case 2:
                     cerrarSesion();
                     break;
-                case 9:
-                    MainApp.mostrarMenu();
-                    break;
                 default:
                     throw new RuntimeException("Opcion invalida");
             }
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
-            mostrarMenu();
+            mostrarMenuAdentro();
         }
     }
 
     private void verItinerarioDeViajes() {
         System.out.println(server.getPersonalAbordo(currentSesion).toString());
-        mostrarMenu();
+        mostrarMenuAdentro();
     }
 
     private void cerrarSesion() {
-        borrarPantalla();
         System.out.println("Sesion cerrada");
-        iniciarSesion();
+        mostrarMenuAfuera();
     }
 
     public  void borrarPantalla() {
