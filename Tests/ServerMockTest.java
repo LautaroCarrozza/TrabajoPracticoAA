ab95784ac3f47f43e688f5af32f547988f8f28d7
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2017-11-10 09:12:03 -0300

    sadasdas

--- Tests/ServerMockTest.java
+++ Tests/ServerMockTest.java
@@ -25,8 +25,8 @@
 //        server.addAeropuerto("aaa", "aaa", "aaa");
 //        server.addTipoDeAvion(1, 1, 1, 1, 1, 1, 1, "a");
 //        server.addAvion("1", "a");
-//        server.addVuelo("aaa", "aaa", 1, 1, 2018, 22, 30, 1, "1", 3, 3);
-//        server.comprarAsiento(1, 3, 1, "A", 1);
+//        server.addVuelo("aaa", "aaa", 1, 1, 2018, 22, 30, 1, "1", 3, 3,100,200,300);
+//        server.comprarAsiento(1, 3, 1, "A", 1,"nombre1",123456789);
 //    }
 //
 //
